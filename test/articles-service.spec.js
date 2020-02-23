d067aaef538222c657d75d74d3f4fd9db2f493ba
Ben Hernandez <benjammin234@gmail.com>
2020-02-23 11:23:17 -0800

    Preparing to create tests

---
+++ test/articles-service.spec.js
@@ -0,0 +1,9 @@
+const ArticlesService = require("../src/articles-service");
+
+describe(`Articles service object`, function() {
+  describe(`getAllArticles()`, () => {
+    it(`resolves all articles from 'blogful_articles' table`, () => {
+      // test
+    });
+  });
+});
