8e1b8df91277d6323231f2fc1cb46fe77a00ec13
Ben Hernandez <benjammin234@gmail.com>
2020-02-23 11:40:42 -0800

    Creates a single functioning test

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -1,9 +1,48 @@
 const ArticlesService = require("../src/articles-service");
+const knex = require("knex");
 
 describe(`Articles service object`, function() {
+  let db;
+  let testArticles = [
+    {
+      id: 1,
+      date_published: new Date("2029-01-22T16:28:32.615Z"),
+      title: "First test post",
+      content: "Blah blah blah"
+    },
+    {
+      id: 2,
+      date_published: new Date("2100-05-22T16:28:32.615Z"),
+      title: "Second test post",
+      content: "Slightly more interesting"
+    },
+    {
+      id: 3,
+      date_published: new Date("1919-12-22T16:28:32.615Z"),
+      title: "Third test post",
+      content: "Nope, just as bland actually"
+    }
+  ];
+  before(() => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DB_URL
+    });
+  });
+
+  before(() => db("blogful_articles").truncate());
+
+  before(() => {
+    return db.into("blogful_articles").insert(testArticles);
+  });
+
+  after(() => db.destroy());
+
   describe(`getAllArticles()`, () => {
     it(`resolves all articles from 'blogful_articles' table`, () => {
-      // test
+      return ArticlesService.getAllArticles(db).then(actual => {
+        expect(actual).to.eql(testArticles);
+      });
     });
   });
 });
